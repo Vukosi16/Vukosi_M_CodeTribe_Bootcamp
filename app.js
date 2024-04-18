f6611c07a1eb85564f2abcad8b72872cd3c3fd35
Vukosi <vukosimohlabini16@gmail.com>
2024-04-18 12:21:47 +0200

    Added JS variables for common uses things like names

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
+const firstName = "Vukosi";
+const surname =  "Mohlabini";
+const fullName = "Vukosi Mohlabini"
+const bio = "Meet Vukosi - A tech junkie, part time model (for Xross Drop only) and programmer extrodinare (wannabe) with a interesting passion for astronomy. When he's not coding up a storm, you'll catch him stargazing and pondering the wonders of the universe. But Vuks' not all about tech and stars; he's also a sports fanatic.<br>Whether he's rooting for the Texas Longhorns on the gridiron (College Football), cheering on Manchester United in soccer, or getting his adrenaline fix with Formula 1 racing, he's always in the game. And amidst all his interests, Vukosi's got one big dream: to visit the CERN Hadron Collider and unlock the mysteries of the cosmos firsthand. With his blend of geekiness and sportsmanship, Vukosi's one fascinating guy."
+const phoneNumber = "0672575232";
+const email = "vukosimohlabini16@gmail.com";
+const address = "1110 Pretorius St Hatfield Pretoria 0083";
+
+const nameHeading = document.querySelector('.nameHeading');
+nameHeading.textContent =   `${fullName}`;
+
+const indexPageDescription = document.querySelector('.description');
+indexPageDescription.textContent = `${bio}`;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const button = document.querySelector("#arrow");
 
 button.addEventListener("click", () => {
